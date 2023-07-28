c2c3e91a1011422604de1fd4032ac88d743e03bb
Yang Zhang <yang@users.noreply.github.com>
2023-07-27 22:22:45 -0700

    Convert to loader

--- pages/plasmic-host.tsx
+++ pages/plasmic-host.tsx
@@ -1,14 +1,7 @@
-import * as React from 'react';
-import { PlasmicCanvasHost } from '@plasmicapp/loader-nextjs';
-import { PLASMIC } from 'plasmic-init';
-import { registerAll } from "@plasmicpkgs/commerce"
-import { registerCommerceProvider } from '@/commerce/shopify';
-import { registerCommerceProvider as registerBigCommerce } from '@/commerce/bigcommerce';
+import * as React from "react";
+import { PlasmicCanvasHost } from "@plasmicapp/loader-nextjs";
+import { PLASMIC } from "plasmic-init";
+
 export default function PlasmicHost() {
   return PLASMIC && <PlasmicCanvasHost />;
 }
-
-registerAll(PLASMIC);
-// registerCommerceProvider(PLASMIC);
-registerBigCommerce(PLASMIC);
-
